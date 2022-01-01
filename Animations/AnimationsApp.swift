4606d1064b040179ca425cfdd7055d4bbc1706d9
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-01 13:34:21 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Vahtee Boo on 01.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
