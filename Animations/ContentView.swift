a30090433e7dab30dd4b8423706f3c6222847139
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 07:43:58 +0200

    Part 2 of 8

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 0.7
+    @State private var animationAmount = 1.0
     
     var body: some View {
-            Button("Tap Me") {
-                animationAmount += 0.3
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(.default, value: animationAmount)
+        Button("Tap Me") {
+//            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+//        .scaleEffect(animationAmount)
+        //            .blur(radius: (animationAmount - 1) * 3)
+        .overlay(Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        //                    .repeatCount(5, autoreverses: true),
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
