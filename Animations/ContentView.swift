cdec12e4c34c1e315cafa03928b6d8ecfa0661c6
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 10:21:18 +0200

    Part 4 of 8

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,28 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        print(animationAmount)
         
-        return VStack {
-            Stepper("Scale amoint", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
+        Button("Tap Me") {
             
-            Spacer()
-                    
-            Button("Tap Me") {
-                animationAmount += 1
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 0))
     }
 }
 
