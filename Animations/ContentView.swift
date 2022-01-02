fd466649dd8aebe802c39318f1569338e2d3fe1f
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 09:54:30 +0200

    Part 3 of 8

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -12,28 +12,24 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-//        .scaleEffect(animationAmount)
-        //            .blur(radius: (animationAmount - 1) * 3)
-        .overlay(Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeOut(duration: 1)
-                        //                    .repeatCount(5, autoreverses: true),
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-        )
-        .onAppear {
-            animationAmount = 2
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amoint", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+                    
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
     }
 }
